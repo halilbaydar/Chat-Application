6077e68e3f53572a739323f4e863027815fa732e
halil <halillbaydar@gmail.com>
2022-08-07 21:50:20 +0300

    cors properties injecteion changed

--- src/main/java/com/chat/config/CorsConfig.java
+++ src/main/java/com/chat/config/CorsConfig.java
@@ -1,19 +1,19 @@
 package com.chat.config;
 
 import com.chat.property.CorsProperties;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
     private static final String MAPPING = "/**";
     private static final String[] METHODS = {"GET", "POST"};
 
-    @Autowired
-    private CorsProperties corsProperties;
+    private final CorsProperties corsProperties;
 
     @Bean
     public WebMvcConfigurer corsConfigurerForProd() {
