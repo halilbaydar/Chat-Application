afcdd60fa9e3fe5fc0d825b93dc710d00e0a559a
= <halillbaydar@gmail.com>
2023-04-20 23:51:33 +0300

    kafka module added

---
+++ mdc-interceptor/src/main/java/com/chat/Constants.java
@@ -0,0 +1,6 @@
+package com.chat;
+
+public class Constants {
+    public static final String CORRELATION_ID_HEADER = "X-Correlation-ID";
+    public static final String CORRELATION_ID_KEY = "correlationID";
+}
