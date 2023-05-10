17bfaeb1b3adecc527ca87ea5dcd2ae982a96a92
= <halillbaydar@gmail.com>
2023-05-10 23:13:25 +0300

    initial notificaiton service modules added

--- notification-service/src/app.module.ts
+++ notification-service/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ModuleWorkflow} from "./modules/workflow/module.workflow";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ModuleWorkflow],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import * as express from 'express'
+import {ExpressAdapter} from "@nestjs/platform-express";
+import * as http from "http";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const server = express()
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+    http.createServer(server).listen(process.env.PORT, () => {
+        console.log(`🚀 app listening on port ${process.env.PORT}!`);
+    })
+    await app.init()
 }
+
 bootstrap();
